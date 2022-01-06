06b05f2fadd6fd2ee786118768def38c7bbc378b
Анатолий Биртанов <a.birtanov@takeoff-staff.ru>
2022-01-06 04:50:39 +0300

    remove alert

--- src/components/Ui/Filter/Filter.tsx
+++ src/components/Ui/Filter/Filter.tsx
@@ -1,6 +1,5 @@
 import React, { FC, useEffect, useState } from 'react';
 import classes from './Filter.module.scss';
-import { AnimatePresence, motion } from 'framer-motion';
 import cn from 'classnames';
 import { Range, RangeProps } from '../Range';
 
@@ -25,22 +24,8 @@ interface FilterParams {
   list: FilterItem[];
 }
 
-const alertAnimation = {
-  initial: { x: '-20px', width: 0 },
-  animate: { x: '10px', width: 'auto' },
-  exit: { x: '-20px', width: 0, transition: { duration: 0.6 } },
-};
-
-const alertTextAnimation = {
-  initial: { opacity: 0, scaleX: 0 },
-  animate: { opacity: 1, scaleX: 1, transition: { duration: 0.6 } },
-  exit: { opacity: 0, scaleX: 0 },
-};
-
 const Filter: FC<FilterProps> = ({ filterProps, range, applyFilter }) => {
   const [active, setActive] = useState<State>({});
-  const [activeBlock, setActiveBlock] = useState<string>('');
-  const [showAlert, setShowAlert] = useState<boolean>(false);
 
   useEffect(() => {
     filterProps.forEach(({ title }) =>
@@ -52,17 +37,10 @@ const Filter: FC<FilterProps> = ({ filterProps, range, applyFilter }) => {
     if (id !== active[category]) {
       setActive({ ...active, [category]: id });
       if (applyFilter) applyFilter(key, category);
-      if (showAlert) setShowAlert(false);
     } else {
-      setShowAlert(!showAlert);
+      setActive({ ...active, [category]: null });
+      if (applyFilter) applyFilter(null, category);
     }
-    if (category !== activeBlock) setActiveBlock(category);
-  };
-
-  const onAlertClick = (category: string) => {
-    setActive({ ...active, [category]: null });
-    if (applyFilter) applyFilter(null, category);
-    setShowAlert(false);
   };
 
   return (
@@ -82,26 +60,6 @@ const Filter: FC<FilterProps> = ({ filterProps, range, applyFilter }) => {
                 >
                   {label}
                 </span>
-                <div className={classes.filter__alertWrapper}>
-                  <AnimatePresence>
-                    {showAlert &&
-                      active[item.title] === id &&
-                      activeBlock === item.title && (
-                        <motion.button
-                          className={classes.filter__alert}
-                          {...alertAnimation}
-                          transition={{ type: 'tween' }}
-                          whileTap={{ scale: 0.95 }}
-                          onClick={() => onAlertClick(item.title)}
-                          onBlur={() => setShowAlert(false)}
-                        >
-                          <motion.span {...alertTextAnimation}>
-                            Remove filter
-                          </motion.span>
-                        </motion.button>
-                      )}
-                  </AnimatePresence>
-                </div>
               </li>
             ))}
           </ul>
